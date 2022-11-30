3c98ccd8acab8076fe83dbac1cff9ae2a51d65f7
Rinsz <rinszz@yandex.ru>
2022-11-30 20:47:10 +0500

    create working models download logic

---
+++ Assets/Scripts/Models/Descriptors/ModelDownloadCard.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using static UnityConstants;
+
+namespace Models.Descriptors
+{
+    public class ModelDownloadCard : MonoBehaviour
+    {
+        public Image modelIcon;
+        public TMP_Text modelName;
+        internal ModelMeta meta;
+
+        public void Download()
+        {
+            while (!Caching.ready)
+            {
+                Debug.Log("Cache is not ready.");
+            }
+
+            var bundleRequest = UnityWebRequest.Get($"https://localhost:7051/api/models/bundles/{meta.Id}/{meta.Version}");
+            using var certHandler = new StubCertHandler();
+            bundleRequest.certificateHandler = certHandler;
+            var requestResult = bundleRequest.SendWebRequest();
+            requestResult.completed += _ =>
+            {
+                var cachePath = Path.Combine(Application.persistentDataPath, "cachedBundles");
+                if (!Directory.Exists(cachePath))
+                    Directory.CreateDirectory(cachePath);
+                var bytes = requestResult.webRequest.downloadHandler.data;
+                using var fileStream = new FileStream(Path.Combine(cachePath, $"{meta.Id}{Unity3dExtension}"), FileMode.Create);
+                fileStream.Write(bytes, 0, bytes.Length);
+
+                
+                using var metaFileStream = new FileStream(Path.Combine(cachePath, $"{meta.Id}.json"), FileMode.Create);
+                using var sw = new StreamWriter(metaFileStream);
+                JsonSerializer.Create().Serialize(sw, meta);
+
+                Debug.Log($"Download completed: {meta.Name}/{meta.ModelGroup}");
+                Debug.Log($"Saved to: {Caching.currentCacheForWriting.path}");
+            };
+        }
+        
+        // TODO Issue cert for api and remove stub
+        private class StubCertHandler : CertificateHandler
+        {
+            protected override bool ValidateCertificate(byte[] certificateData)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Models/ExternalModelsBrowser.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Models.Descriptors;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Models
+{
+    public class ExternalModelsBrowser : MonoBehaviour
+    {
+        public GameObject ModelDownloadCardPrefab;
+
+        private List<ModelGroupCard> modelGroups;
+
+        private readonly List<GameObject> createdCards = new();
+
+        private void OnEnable() => DownloadContent();
+
+        private void OnDisable()
+        {
+            foreach (var card in createdCards)
+                Destroy(card);
+        }
+
+        public void DownloadContent()
+        {
+            var metasRequest = UnityWebRequest.Get("https://localhost:7051/api/models/metas");
+            using var certHandler = new StubCertHandler();
+            metasRequest.certificateHandler = certHandler;
+            var requestResult = metasRequest.SendWebRequest();
+            requestResult.completed += _ => CreateCards(requestResult.webRequest.downloadHandler.text);
+        }
+
+        private void CreateCards(string metasJson)
+        {
+            using var tr = new StringReader(metasJson);
+            using var jtr = new JsonTextReader(tr);
+            var metas = JsonSerializer.CreateDefault().Deserialize<List<ModelMeta>>(jtr);
+
+            foreach (var meta in metas)
+            {
+                var rawObject = Instantiate(ModelDownloadCardPrefab, this.transform, false);
+                var modelDownloadCard = rawObject.GetComponent<ModelDownloadCard>();
+                modelDownloadCard.meta = meta;
+                modelDownloadCard.modelName.text = meta.Name;
+                createdCards.Add(rawObject);
+                LoadImage(meta, modelDownloadCard);
+            }
+        }
+
+        private static void LoadImage(ModelMeta meta, ModelDownloadCard modelDownloadCard)
+        {
+            var imageRequest = UnityWebRequestTexture
+                .GetTexture($"https://localhost:7051/api/models/images/{meta.Id}/{meta.Version}");
+            using var ch = new StubCertHandler();
+            imageRequest.certificateHandler = ch;
+            var imageRequestResult = imageRequest.SendWebRequest();
+            imageRequestResult.completed += _ => SetImageForCard(imageRequestResult.webRequest, modelDownloadCard);
+        }
+
+        private static void SetImageForCard(UnityWebRequest webRequest, ModelDownloadCard modelDownloadCard)
+        {
+            var texture = DownloadHandlerTexture.GetContent(webRequest);
+            var image = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+                new Vector2(texture.width / 2, texture.height / 2));
+
+            modelDownloadCard.modelIcon.sprite = image;
+        }
+
+        // TODO Issue cert for api and remove stub
+        private class StubCertHandler : CertificateHandler
+        {
+            protected override bool ValidateCertificate(byte[] certificateData)
+            {
+                return true;
+            }
+        }
+    }
+}
